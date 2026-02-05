a1682d09e74baf6393b66a1a7a1d5512e8c3a6f5
Said De Oleo <saiddeoleo26@gmail.com>
2026-02-05 15:24:58 -0400

    fix: hola_mundo, alerta and test

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const { execSync } = require("child_process");
 
 const output = execSync("node hola_mundo.js").toString().trim();
 
-if (output === "hola mundo") {
+if (output === "¡Hola Mundo!") {
     console.log("✅ Test passed");
 } else {
     console.error("❌ Test failed");
